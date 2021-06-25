1a317e20ee15fc2cfff270668abac9cb10eaab38
VeronikaMarss <VeronikaMarss@users.noreply.github.com>
2021-06-25 19:48:29 +0300

    ДЗ по второму уроку. 1. Проверка у меня не запускается, не понимаю почему.. смотрела по лекции. 2. Второе задание скорее всего тоже не верно, но может вы подталкнете куда двигатся. 3. Не могу понять как в одном классе работают несколько ДЗ (задач), В одном классе мы создаем методы и в них выполняются задачи ? 4. Не смогла примерить функцию debug. Если я выделаю строку и наживаю - происходит тоже самое что и нажимая run..
    р.s буду еще раз пересматривать лекции 2 и 3.

---
+++ Lesson2/src/homework2.java
@@ -0,0 +1,33 @@
+import static java.lang.Math.max;
+
+public class homework2 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+        int c = a + b;
+        System.out.println(c);
+    }
+
+    public static int c(int a, int b) {
+        long c = a + (Long) b;
+        System.out.println(c);
+        if (c > Integer.MAX_VALUE) {
+            return -1;
+        }
+        return (int) c;
+    }
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        boolean min a>b;
+        boolean max b>a;
+        if (!min && max) {
+            System.out.println(max);
+        }
+
+    }
+}
+
+
+
