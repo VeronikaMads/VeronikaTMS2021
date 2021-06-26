9e8a225995d46b2c8a588cfdd8af9507bd4a2f92
VeronikaMarss <VeronikaMarss@users.noreply.github.com>
2021-06-26 17:59:45 +0300

    ДЗ по второму уроку /2 1. в первой задаче исправила ошибки. 2. в второй задаче не смогла написать код с помощью boolean. (подскажите в чем ошибка) и проверку пока не понимаю как сдать. 3. четвертая задача решина с помощью интернета. 4. за 5 еще не садилась. попываюсь сегодня/завтра.

--- Lesson2/src/homework2.java
+++ Lesson2/src/homework2.java
@@ -6,10 +6,11 @@ public class homework2 {
         int b = 200;
         int c = a + b;
         System.out.println(c);
+        c(100, 200);
     }
 
     public static int c(int a, int b) {
-        long c = a + (Long) b;
+        long c = a + (long) b;
         System.out.println(c);
         if (c > Integer.MAX_VALUE) {
             return -1;
@@ -17,17 +18,10 @@ public class homework2 {
         return (int) c;
     }
 
-    public static void main(String[] args) {
-        int a = 4;
-        int b = 5;
-        boolean min a>b;
-        boolean max b>a;
-        if (!min && max) {
-            System.out.println(max);
-        }
-
-    }
 }
+
+
+
 
 
 
