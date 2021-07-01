79309daadd263697c69318316ab36b1a4cf03b25
VeronikaMarss <VeronikaMarss@users.noreply.github.com>
2021-07-02 00:07:35 +0300

    ДЗ

--- Lesson3/src/HomeWork2.java
+++ Lesson3/src/HomeWork2.java
@@ -15,4 +15,17 @@ public class HomeWork2 {
         System.out.println("Результат : " + numder);
     }
 }
-
+/**
+ * На вход приходит массив целых чисел типа int
+ * Необходимо найти количество нечетных элементов в массиве и вернуть значение в метод main,
+ * в котором это значение распечатается на консоль.
+ */
+//    public static int calculateCountOfOddElementsInMatrix(int[] ints) {
+//        int count = 0;
+//        for (int i : ints) {
+//            if (i % 2 != 0) {  // Если при делении i на 2 остаток не равняется 0 - count ++ (неч.числа) (i%2 == 0) - четные
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
