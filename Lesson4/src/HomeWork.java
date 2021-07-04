27919c3c3754bfc3f613ae5efb8e98508e6e3ea6
VeronikaMarss <VeronikaMarss@users.noreply.github.com>
2021-07-04 20:14:26 +0300

    ДЗ2

--- Lesson4/src/HomeWork.java
+++ Lesson4/src/HomeWork.java
@@ -1,20 +1,24 @@
 import javax.imageio.spi.ImageInputStreamSpi;
+import java.util.*;
 
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println(ameba(new int[]{3, 6, 9, 12, 15, 18, 21, 24}));
- //       summa();
-        ImageInputStreamSpi();
+//        System.out.println(ameba(new int[]{3, 6, 9, 12, 15, 18, 21, 24}));
+//       summa();
+//        ImageInputStreamSpi();
+//        printMatrix();
+//        ArraySort();
+        Array1();
     }
     //  1) Одноклеточная амеба каждые 3 часа делится на 2 клетки. Определить,
 //   сколько амеб будет через 3, 6, 9, 12,..., 24 часа
-    public static int ameba(int []ints){
-        int count =0;
-        for (int i = 1; i < ints.length ; i++) {
-
-
-        }
-    }
+//    public static int ameba(int []ints){
+//        int count =0;
+//        for (int i = 1; i < ints.length ; i++) {
+//
+//
+//        }
+//    }
 
 
     // 2) Напишите реализацию метода summ(int a, int b), вычисляющий a*b, не пользуясь операцией
@@ -42,18 +46,69 @@ public class HomeWork {
 //            * *        * *
 //              *        *
 
-    public static void ImageInputStreamSpi() {
-        int mass[][]=new int[4][4];
+//    public static void ImageInputStreamSpi() {
+//        int mass[][] = new int[4][4];
+//        for (int i = 0; i < mass.length; i++) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print(" * ");
+//            }
+//            System.out.println(" \n ");
+//        }
+//    }
+//        4)В переменную записываем число.
+//        Надо вывести на экран сколько в этом числе цифр и положительное оно или отрицательное.
+//        Например, Введите число: 5
+//        "5 - это положительное число, количество цифр = 1"
+
+//    public static void printMatrix() {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите значение a: ");
+//        int a = scanner.nextInt();
+//        if (a > 0) {
+//            System.out.println(a + " - это положительное число ");
+//        } else {
+//            System.out.println(a + " - это отрицательное число ");
+//        }
+//        System.out.println((a+"").length());
+//        }
+
+
+//        5) Создайте массив из всех нечётных чисел от 1 до 100, выведите его на экран в строку,
+//        а затем этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 ... 7 5 3 1).
+
+//    public static void ArraySort() {
+//        int mass[] = new int[100];
+//        for (int i = 0; i < 100; i++) {
+//            if (i % 2 != 0) {
+//                System.out.printf("%5d", i);
+//            }
+////            Arrays.sort(i);
+////            System.out.println(Arrays.toString());
+//        }
+//        System.out.println(" ");
+//        for (int i = mass.length-1; i >=0 ; i--) {
+//            System.out.printf("%5d", i);
+//
+//        }
+//    }
+
+//        6) Создайте массив из int[] mass = new int[12]; Рандомно заполните его значениями от 0 до 15.
+//        Определите какой элемент является в этом массиве максимальным и сообщите индекс его последнего вхождения в массив.
+//        Пример: {3,4,5,62,7,8,4,-5,7,62,5,1} Максимальный элемент 62, индекс его последнего вхождения в массив = 10
+
+    public static void Array1(){
+        int[] mass = new int[12];
+        Random random = new Random();
         for (int i = 0; i < mass.length; i++) {
-            for (int j = 0; j < mass[i].length; j++) {
-                if ()
-                System.out.printf("%5d", mass[i][j]);
-            }
-            System.out.println();
+            mass[i] = random.nextInt(15);
+            System.out.printf("%5d",mass[i]);
+
         }
 
+    }
+
 }
 
 
-}
+
 
