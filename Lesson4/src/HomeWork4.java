9d8b713d1d0580dc9c25d423cf5a308ee6263f12
VeronikaMarss <VeronikaMarss@users.noreply.github.com>
2021-07-07 10:21:23 +0300

    Добрый день! ДЗ по 4 уроку. Это пока все с чем я смогла разобраться. Вопросы остались по выводу мак. числа. я не уверена что задача 6 решена у меня верно. почти тоже самое в 8 задаче с MIN_VALUE. я сделала так как в лекции, но все равно максимальное значение не выводит.(получается массив без 0)
    Так же у меня проблемы с выводом. массива. Задача 10 . (я не могу понять как вывести матрицу отдельно от ранее созданной) Вроде как получается если долго сидеть, пересматривать лекции, но каждый раз по разному. то с помощью  System.out.printF ,Arrays.toString или новой строки "\n".
    в общем до конца нет понимая)
    
    ДЗ по урокам 5 и 6 (и сегодняшнему) буду их разбирать после среды. Обязательно к понедельнику догоню остальных.(пока хочу понять дз 4)

--- Lesson4/src/HomeWork4.java
+++ Lesson4/src/HomeWork4.java
@@ -4,48 +4,48 @@ import java.util.Scanner;
 
 public class HomeWork4 {
     public static void main(String[] args) {
-//        int amoeba = 1;
-//        kolichectvoAmeb(amoeba);
-//        summa(3,4);
-//        summRec(4,5);
-//        ImageInputStreamSpi();
-//        printMatrix();
-//        ArraySort();
-//        Array1();
-//        Array2();
-//        Array3();
-//        Arrays4();
+        int amoeba = 1;
+        kolichectvoAmeb(amoeba);
+        summa(3,4);
+ //       summRec(4,5);
+        ImageInputStreamSpi();
+        printMatrix();
+        ArraySort();
+        Array1();
+        Array2();
+        Array3();
+        Arrays4();
         Arrays5();
 
     }
     //  1) Одноклеточная амеба каждые 3 часа делится на 2 клетки. Определить,
 //   сколько амеб будет через 3, 6, 9, 12,..., 24 часа
 
-//    public static void kolichectvoAmeb(int amoeba) {
-//        for (int i = 0; i < 24; i += 3) {
-//            amoeba = amoeba * 2;
-//            System.out.print(amoeba+ "  " );
-//        }
-//    }
+    public static void kolichectvoAmeb(int amoeba) {
+        for (int i = 0; i < 24; i += 3) {
+            amoeba = amoeba * 2;
+            System.out.print(amoeba+ "  " );
+        }
+    }
 
 //     2) Напишите реализацию метода summ(int a, int b), вычисляющий a*b, не пользуясь операцией
 // умножения, где a и b целые числа, вызовите метод summ  в методе main и распечатайте на консоль.
 
-//    public static void summa(int a, int b) {
-//        int result = 0;
-//        if (a != 0 || b != 0) {
-//            int A = Math.abs(a);
-//            int B = Math.abs(b);
-//            for (int i = 0; i < B; i++) {
-//                result = result + A;
-//
-//            }
-//            if (a > 0 ^ b > 0) {
-//                result = result - result;
-//            }
-//        }
-//        System.out.println(result);
-//    }
+    public static void summa(int a, int b) {
+        int result = 0;
+        if (a != 0 || b != 0) {
+            int A = Math.abs(a);
+            int B = Math.abs(b);
+            for (int i = 0; i < B; i++) {
+                result = result + A;
+
+            }
+            if (a > 0 ^ b > 0) {
+                result = result - result;
+            }
+        }
+        System.out.println(result);
+    }
 
 //    public static int summRec(int a, int b) {
 //        if (a ==0 || b==0) {
@@ -77,112 +77,112 @@ public class HomeWork4 {
 //            * *        * *
 //              *        *
 
-//    public static void ImageInputStreamSpi() {
-//        int mass[][] = new int[4][4];
-//        for (int i = 0; i < mass.length; i++) {
-//            for (int j = 0; j < i; j++) {
-//                System.out.print(" * ");
-//            }
-//            System.out.println(" \n ");
-//        }
-//    }
+    public static void ImageInputStreamSpi() {
+        int mass[][] = new int[4][4];
+        for (int i = 0; i < mass.length; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println(" \n ");
+        }
+    }
 
 //        4)В переменную записываем число.
 //        Надо вывести на экран сколько в этом числе цифр и положительное оно или отрицательное.
 //        Например, Введите число: 5
 //        "5 - это положительное число, количество цифр = 1"
 
-//    public static void printMatrix() {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Введите значение a: ");
-//        int a = scanner.nextInt();
-//        if (a > 0) {
-//            System.out.println(a + " - это положительное число ");
-//        } else if (a < 0) {
-//            System.out.println(a + " - это отрицательное число ");
-//        } else {
-//            System.out.println("Вы ввели 0 . Введите положительное или отрицательное число ");
-//        }
-//
-//        System.out.println((Math.abs(a) + " ").length());
-//    }
+    public static void printMatrix() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите значение a: ");
+        int a = scanner.nextInt();
+        if (a > 0) {
+            System.out.println(a + " - это положительное число ");
+        } else if (a < 0) {
+            System.out.println(a + " - это отрицательное число ");
+        } else {
+            System.out.println("Вы ввели 0 . Введите положительное или отрицательное число ");
+        }
+
+        System.out.println((Math.abs(a) + " ").length());
+    }
 
 
 //        5) Создайте массив из всех нечётных чисел от 1 до 100, выведите его на экран в строку,
 //        а затем этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 ... 7 5 3 1).
 
-//    public static void ArraySort() {
-//        int mass[] = new int[100];
-//        for (int i = 0; i < 100; i++) {
-//            if (i % 2 != 0) {
-//                System.out.printf("%5d", i);
-//            }
-////            Arrays.sort(i);
-////            System.out.println(Arrays.toString());
-//        }
-//        System.out.println(" ");
-//        for (int i = mass.length-1; i >=0 ; i--) {
-//            System.out.printf("%5d", i);
-//
-//        }
-//    }
+    public static void ArraySort() {
+        int mass[] = new int[100];
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 != 0) {
+                System.out.printf("%5d", i);
+            }
+//            Arrays.sort(i);
+//            System.out.println(Arrays.toString());
+        }
+        System.out.println(" ");
+        for (int i = mass.length-1; i >=0 ; i--) {
+            System.out.printf("%5d", i);
+
+        }
+    }
 
 //        6) Создайте массив из int[] mass = new int[12]; Рандомно заполните его значениями от 0 до 15.
 //        Определите какой элемент является в этом массиве максимальным и сообщите индекс его последнего вхождения в массив.
 //        Пример: {3,4,5,62,7,8,4,-5,7,62,5,1} Максимальный элемент 62, индекс его последнего вхождения в массив = 10
 
-//    public static void Array1() {
-//        int[] mass = new int[12];
-//        int max = 15;
-//        int maxIndex = 0;
-//        Random random = new Random();
-//        for (int i = 0; i < mass.length; i++) {
-//            mass[i] = random.nextInt(16);
-//            System.out.printf("%5d", mass[i]);
-//            if (mass[i] >= max) {
-//                max = mass[i];
-//                maxIndex = i;
-//            }
-//
-//        }
-//        System.out.println("  ");
-//        System.out.println("Индекс максимального элемента: " + maxIndex);
-//        System.out.println("Значение максимального элемента: " + max);
-//    }
+    public static void Array1() {
+        int[] mass = new int[12];
+        int max = 15;
+        int maxIndex = 0;
+        Random random = new Random();
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = random.nextInt(16);
+            System.out.printf("%5d", mass[i]);
+            if (mass[i] >= max) {
+                max = mass[i];
+                maxIndex = i;
+            }
+
+        }
+        System.out.println("  ");
+        System.out.println("Индекс максимального элемента: " + maxIndex);
+        System.out.println("Значение максимального элемента: " + max);
+    }
 
 //        7) Создайте массив размера 20, заполните его случайными целыми чиселами из отрезка от 0 до 20.
 //        Выведите массив на экран в строку. Замените каждый элемент с нечётным индексом на ноль.
 //        Снова выведете массив на экран на отдельной строке.
 
-//    public static void Array2() {
-//        int[] mass1 = new int[20];
-//        Random random = new Random();
-//        for (int i = 0; i < mass1.length; i++) {
-//            mass1[i] = random.nextInt(21);
-//            System.out.print(mass1[i] + "  ");
-//            if (i % 2 != 0) {
-//                mass1[i] = 0;
-//            }
-//
-//        }
-//        System.out.println("  ");
-//        System.out.println(Arrays.toString(mass1));
-//    }
+    public static void Array2() {
+        int[] mass1 = new int[20];
+        Random random = new Random();
+        for (int i = 0; i < mass1.length; i++) {
+            mass1[i] = random.nextInt(21);
+            System.out.print(mass1[i] + "  ");
+            if (i % 2 != 0) {
+                mass1[i] = 0;
+            }
+
+        }
+        System.out.println("  ");
+        System.out.println(Arrays.toString(mass1));
+    }
 
 //        8) Найти максимальный элемент в массиве {4,5,0,23,77,0,8,9,101,2} и поменять его местами с нулевым элементом
 
-//    public static void Array3() {
-//        int[] mass3 = {4, 5, 0, 23, 77, 0, 8, 9, 101, 2};
-//        int MAX = Integer.MIN_VALUE;
-//        int indexMAX = -1;
-//        for (int i = 0; i < mass3.length; i++) {
-//            if (mass3[i] > MAX) {
-//                MAX = mass3[i];
-//                indexMAX = i;
-//                System.out.println(" " + MAX);
-//            }
-//        }
-//    }
+    public static void Array3() {
+        int[] mass3 = {4, 5, 0, 23, 77, 0, 8, 9, 101, 2};
+        int MAX = Integer.MIN_VALUE;
+        int indexMAX = -1;
+        for (int i = 0; i < mass3.length; i++) {
+            if (mass3[i] > MAX) {
+                MAX = mass3[i];
+                indexMAX = i;
+                System.out.println(" " + MAX);
+            }
+        }
+    }
 
 
 
@@ -192,9 +192,9 @@ public class HomeWork4 {
 //        Пример: {0,34,46,31,20,1,28}
 //        Массив не имеет повторяющихся элементов
 
-//    public static void Arrays4(){
-//
-//    }
+    public static void Arrays4(){
+
+    }
 
 
 //        10) Создаём квадратную матрицу, размер вводим с клавиатуры.
