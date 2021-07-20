7c5ffd254c3295f51a3ba5c2f56e5d3bf1ca96b2
VeronikaMarss <VeronikaMarss@users.noreply.github.com>
2021-07-20 12:35:00 +0300

    commit исправляла Компьютер

--- homeWork5/src/main/java/com/home/model/Main.java
+++ homeWork5/src/main/java/com/home/model/Main.java
@@ -5,7 +5,7 @@ import com.home.model.Computer;
 
 public class Main {
     public static void main(String[] args) {
-        Computer computer = new Computer("Intel Core i3", 8, 8, 1);
+        Computer computer = new Computer("Intel Core i3", 8, 8, 5);
         computer.info();
         computer.on();
         computer.off();
