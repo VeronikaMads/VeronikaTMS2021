49f5418dd6669eaf0022649105da3bc1f7f0750c
VeronikaMarss <VeronikaMarss@users.noreply.github.com>
2021-07-25 22:04:28 +0300

    commit 7

--- homeWork7/src/main/java/com/home/metod/Calculation.java
+++ homeWork7/src/main/java/com/home/metod/Calculation.java
@@ -4,7 +4,6 @@ import com.home.heir2.Cargo;
 import com.home.heir2.Passenger;
 import com.home.heir3.Civil;
 import com.home.heir3.Military;
-import com.home.parentClass.Transport;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,14 +13,11 @@ import java.util.Scanner;
 
 @Getter
 @Setter
-@SuperBuilder
 @ToString(callSuper = true)
-public abstract class Calculation implements Method {
+@SuperBuilder
+public class Calculation {
 
-    @Override
-    public void cargoWeightInfo(Transport transport) { // вызываем метод
-        Method.super.cargoWeightInfo(transport);
-        Cargo cargo = (Cargo) transport;
+    public void cargoWeightInfo(Cargo cargo) { // вызываем метод
         double loadCapacity = cargo.getLoadCapacity();// связывем с классом (откуда будем брать информацию о грузо-ти)
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите массу груза в кг, которую хотите загурзить в грузовик :");
@@ -33,10 +29,7 @@ public abstract class Calculation implements Method {
         }
     }
 
-    @Override
-    public void civilInfo(Transport transport) {
-        Method.super.civilInfo(transport);
-        Civil civil = (Civil) transport;
+    public void civilInfo(Civil civil) {
         int numbOfPassengers = civil.getNumbOfPassengers();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите количество пассажиров, которое вы хотите посадить в гражданский самолет:");
@@ -49,22 +42,16 @@ public abstract class Calculation implements Method {
 
     }
 
-    @Override
-    public void militaryCatapultInfo(Transport transport) {
-        Method.super.militaryCatapultInfo(transport);
-        Military military = (Military) transport;
-        if (military.isEjectionSystem()){
+    public void militaryCatapultInfo(Military military) {
+        if (military.isEjectionSystem()) {
             System.out.println("Катапультирование прошло успешно");
-        }else {
+        } else {
             System.out.println("У вас нет такой системы");
         }
 
     }
 
-    @Override
-    public void militaryRacketInfo(Transport transport) {
-        Method.super.militaryRacketInfo(transport);
-        Military military = (Military) transport;
+    public void militaryRacketInfo(Military military) {
         int missile = military.getMissile();
         if (missile == 0) {
             System.out.println("Боеприпасы отсутствуют");
@@ -73,25 +60,23 @@ public abstract class Calculation implements Method {
         }
     }
 
-    @Override
-    public void passengerDistanceInfo(Transport transport, double time,double distance) {
-        Method.super.passengerDistanceInfo(transport, time,distance);
-        Passenger passenger =(Passenger) transport;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите время автомобиля в пути ЧЧ,ММ :");
-        time = scanner.nextInt(); // присваиваем значение переменной время
-        distance =passenger.getSpeed()*time;
-        System.out.println("За время ЧЧ,ММ " +time+ "автомобиль" +passenger.getBrand()+"\n"+ "двигаясь с максимальной скоростью "
-     +passenger.getSpeed()+ "\n" + "проедет" +distance+ "км и израсходует"  (xxx) литров топлива");
+
+    public void passengerAllInfo(Passenger passenger, double time) {
+        System.out.println("За время ЧЧ,ММ " + time + "автомобиль" + passenger.getBrand() + "\n" + "двигаясь с максимальной скоростью "
+                + passenger.getSpeed() + "\n" + "проедет" + distance(passenger, time) + "км и израсходует" + calculationFuel(passenger, time) + "литров топлива");
+
+
     }
 
-    @Override
-    public void passengerPetrolInfo(Transport transport, double petrol) {
-        Method.super.passengerPetrolInfo(transport, petrol);
-        Passenger passenger =(Passenger) transport;
-        double time = passenger.getFuelConsumption();
-        petrol =((Passenger) transport).getFuelConsumption()*
+    private double distance(Passenger passenger, double time) { // считаем расстояние
+        return passenger.getSpeed() * time;
     }
+
+    private double calculationFuel(Passenger passenger, double time) { // считаем количесвто бензина
+        return distance(passenger, time) * passenger.getFuelConsumption();
+    }
+
 }
+
 
 
