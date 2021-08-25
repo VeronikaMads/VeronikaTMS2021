ac77c56ba20d59fc39c7d948f396cbe779a1e5e0
VeronikaMarss <VeronikaMarss@users.noreply.github.com>
2021-08-25 21:21:32 +0300

    14 commit Библиотеку только что начала, доделаю чуть позже..

---
+++ homeWork14/src/main/java/library/EmailAddress.java
@@ -0,0 +1,6 @@
+package library;
+// - EmailAddress  с полями электронный адрес, дополнительная информация
+public class EmailAddress {
+    private String email;
+    private String AddInformation;
+}
--- homeWork14/src/main/java/streamAPI/Main.java
+++ homeWork14/src/main/java/streamAPI/Main.java
@@ -1,12 +1,5 @@
 package streamAPI;
 
-import org.apache.commons.lang3.StringUtils;
-import org.intellij.lang.annotations.Language;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,27 +13,20 @@ public class Main {
         methodsMain.checkEvenNumbers();
 
 //      * 3) Вывести список имен, но с первой заглавной буквой. список имен: "john", "arya", "sansa"
-
+        methodsMain.checkListNames();
 
 //      * 4) Создаем класс Car с полями number (номер авто) и year (год выпуска).
-        List<CarMain> cars = Arrays.asList(
-                new CarMain("AA1111BX", 2007),
-                new CarMain("AK5555IT", 2010),
-                new CarMain(null, 2012),
-                new CarMain("", 2015),
-                new CarMain("AI3838PP", 2017));
 //     * Необходимо вывести все не пустые номера машин, выпущенных после 2010 года
-        cars.stream()
-                .filter(carMain -> carMain.getYear()>2010)
-                .map(CarMain::getNumber)
-                .filter(carMain->carMain != null && !carMain.isEmpty())
-                .forEach(System.out::println);
+        methodsMain.getCars(MethodsMain.getCarsList());
 
 
 //     * 5) Задан список сотрудников, которые хранятся в коллекции List<Person>.
 //     * Объект Person содержит имя и фамилию(по желанию другие данные)
 //     *  a) написать метод, который отображает все фамилии, начинающиеся на букву "Д".
 //     *  Внимание! метод должен вернуть объект Optional<String>.
+
+        methodsMain.getPersons(MethodsMain.getPersonsList());
+
 //     *  b*)Со звездочкой. Написать метод, который будет выводить в столбик первую букву фамилии и рядом количество сотрудников,
 //     *  у которых фамилия начинается с этой буквы.
 //     *  Пример: Антонов, Петров, Сидоров, Александров...
@@ -48,6 +34,7 @@ public class Main {
 //     *  A - 2 сотрудника
 //                *  П - 1 сотрудник
 //                *  С - 1 сотрудник
+
     }
 
 }
