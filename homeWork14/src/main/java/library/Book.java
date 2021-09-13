8bc0edb85f911b6d27b68c4e89eb5a3d54e8fef9
VeronikaMarss <VeronikaMarss@users.noreply.github.com>
2021-09-13 23:54:05 +0300

    commit библиотка

--- homeWork14/src/main/java/library/Book.java
+++ homeWork14/src/main/java/library/Book.java
@@ -13,5 +13,5 @@ import lombok.ToString;
 public class Book {
     private String author;
     private String title;
-    private int yearOfPublishing;
+    private Integer yearOfPublishing;
 }
--- homeWork14/src/main/java/library/Reader.java
+++ homeWork14/src/main/java/library/Reader.java
@@ -1,6 +1,7 @@
 package library;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.util.ArrayList;
@@ -9,32 +10,32 @@ import java.util.Objects;
 
 //* Для читателя библиотеки будем хранить ФИО и электронный адрес. Каждый читатель может взять в библиотеке одну или несколько книг.
 @Getter
-@ToString
+@Setter
+
 public class Reader {
     private String fio; //ФИО
-    private EmailAddress email; // электронный адрес
+    private String email; // электронный адрес
     private boolean acceptance; //флаг согласия на рассылку
     private List<Book> books; //взятые книги
 
-    public Reader(String fio, EmailAddress email, boolean acceptance) {
+    public Reader(String fio, String email, boolean acceptance) {
         this.fio = fio;
         this.email = email;
         this.acceptance = acceptance;
-        this.books = new ArrayList<>();
+        this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Reader reader = (Reader) o;
+        return fio.equals(reader.fio);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fio);
     }
 }
-
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass())
-//            return false;
-//        Reader reader = (Reader) o;
-//        return fio.equals(reader.fio);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(fio);
-//    }
-//}
